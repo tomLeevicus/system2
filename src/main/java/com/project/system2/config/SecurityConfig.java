e3cf51c0eb5271b2cdaf52d9f563e8754ef44152
tomLeevicus <49803463+tomLeevicus@users.noreply.github.com>
2025-02-19 20:44:46 +0800

    更新
    2025年2月19日20:44:45

--- src/main/java/com/project/system2/config/SecurityConfig.java
+++ src/main/java/com/project/system2/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.project.system2.config;
 
+import com.project.system2.security.filter.JwtAuthenticationTokenFilter;
+import com.project.system2.security.handle.AuthenticationEntryPointImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,8 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.project.system2.security.filter.JwtAuthenticationTokenFilter;
-import com.project.system2.security.handle.AuthenticationEntryPointImpl;
 import com.project.system2.security.handle.LogoutSuccessHandlerImpl;
 import lombok.RequiredArgsConstructor;
 
